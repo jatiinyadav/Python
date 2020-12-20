370e8187a721b25fea725f442a59b30aa846e725
jatiinyadav <jatin27yadav@gmail.com>
2020-12-20 23:50:20 +0530

    update

--- dict.py
+++ dict.py
@@ -1,8 +1,8 @@
 import math as m
 a = m.sqrt(337)
 print(a)
-values = ["1","2","3","4"]
-names = ["j", "s", "t"]
+values = ["1","2","3"]
+names = ["I'm", "Jatin", "Yadav"]
 des = dict(zip(values,names))
 print(des)
 
