83e1d30e1a377c4c592eafb89ae36b5846ef18c9
jatiinyadav <jatin27yadav@gmail.com>
2020-12-20 23:53:51 +0530

    update1

--- forelse.py
+++ forelse.py
@@ -1,7 +1,7 @@
-nums = [2,4,7,9,10]
+nums = [2,4,5,9,10]
 for j in nums:
     if (j%5==0):
-        print(j)
+        print('The no. ' +  str(j) +  ' is a multiple of 5' )
         break
 else: 
     print("not") 
