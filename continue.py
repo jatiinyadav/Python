65e6309f7b05d011db8603196f1401fb4f7c57b0
jatiinyadav <jatin27yadav@gmail.com>
2020-12-29 00:45:46 +0530

    update2

--- continue.py
+++ continue.py
@@ -8,3 +8,11 @@ for i in list:
     if(i%3==0 or i%5==0):
         continue
     print(i)
+
+print("-------")
+
+list = [1,2,3,4,5,6]
+for j in list:
+    if(j%6!= 0):
+        continue
+    print(j, end="9 ")
