21472047454ab4b79835a39361178061d01e023c
jatiinyadav <jatin27yadav@gmail.com>
2020-12-29 23:59:17 +0530

    update6

--- pyth.py
+++ pyth.py
@@ -1,3 +1,4 @@
 x = int(input("Enter 1st Number "))
 y = int(input("Enter 2nd Number "))
-print("The sum is : " + str(x + y))
+z = int(input("Enter 3rd Number "))
+print("The sum is : " + str(x + y + z))
