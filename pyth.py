7c3cff480ab7cb07cff893e69984fb8025c4bc03
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-02 23:53:21 +0530

    update14

--- pyth.py
+++ pyth.py
@@ -2,3 +2,8 @@ x = int(input("Enter 1st Number "))
 y = int(input("Enter 2nd Number "))
 z = int(input("Enter 3rd Number "))
 print("The sum is : " + str(x + y + z))
+
+
+name = "Hey There !!"
+name.replace('y', 'i')
+print(name)
