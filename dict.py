84000ad2467f6d85c03157469674b6cc11534baf
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-05 23:22:21 +0530

    update23

--- dict.py
+++ dict.py
@@ -6,3 +6,11 @@ names = ["I'm", "Jatin", "Yadav"]
 des = dict(zip(values,names))
 print(des)
 
+
+l1 = ['1','2','3']
+l2 = ['4','5','6']
+
+for i in l1:
+    for j in l2:{
+        print(i,j)
+    }
