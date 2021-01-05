487cd8ade5c07125b447db3680626cdb8fc1e69e
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-05 23:50:59 +0530

    update29

--- break.py
+++ break.py
@@ -7,7 +7,7 @@ av = 3
 x = int(input("Enter the no. of candies you want?\n"))
 i = 1
 for i in range(x):
-    if (av >= x ):
+    if (x <= av ):
        print("Candy")
     else:
         print("Sorry, we only have " + str(av) + "  no. of candies")
@@ -18,7 +18,7 @@ print("Have a great day ahead")
 print("----------")
 
 
-lit = [2,4,5,6,9]
+lit = [2,5,6,9]
 for i in lit:
     if (i%2==0):
         print(i, "Even")
@@ -26,3 +26,18 @@ for i in lit:
         print(i, "Odd")
 
 print("----------")
+
+
+av = 3 
+candy = int(input("Enter The no. of candies you want?\n"))
+i = 1
+for i in range(candy):
+    if (candy<=av):
+        if (candy%2==0):
+            print("Here are your even no. of candies")
+        else:
+            print("Here are your odd no. of candies")
+    else:
+        print("Sorry, we only have " + str(av) + "  no. of candies")
+        break
+print("Have a great day ahead")
