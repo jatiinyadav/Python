ab01117fe46487563da5dd70fb36e9fc51c6493d
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-05 23:33:34 +0530

    update26

--- pyth.py
+++ pyth.py
@@ -1,14 +1,21 @@
+"""
 x = int(input("Enter 1st Number "))
 y = int(input("Enter 2nd Number "))
 z = int(input("Enter 3rd Number "))
 print("The sum is : " + str(x + y + z))
-
+"""
 
 name = "Hey There !!"
-print(name.replace('y', 'i'))
+print(name.replace('ey', 'ii'))
 
 print(name.count("Hey There!!"))
 
 print(name.lower())
 
 print(name.upper())
+
+cars = ['Lamborghini', 'Ferrari', 'BMW', 'Audi']
+cars.insert(1, 'Mercedes')
+print(cars)
+cars.sort()
+print(cars)
