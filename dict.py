327bd33b4fb07b6ee5cc4f3ee97cf18b3fbf6316
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-05 23:31:40 +0530

    update25

--- dict.py
+++ dict.py
@@ -6,6 +6,7 @@ names = ["I'm", "Jatin", "Yadav"]
 des = dict(zip(values,names))
 print(des)
 
+print("--------")
 
 l1 = ['1','2','3']
 l2 = ['4','5','6']
@@ -14,8 +15,12 @@ for i in l1:
     for j in l2:
         print(i,j)
 
+print("--------")
+
 i = 1
 n = 2
 while(i<=5):
     print(n," * ", i , " = " ,  n*i)
     i +=1
+
+print("--------")
