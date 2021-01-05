a7bcf26aaae459356f9827ee60b0819ff7bfd4d4
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-05 23:30:47 +0530

    update24

--- dict.py
+++ dict.py
@@ -11,6 +11,11 @@ l1 = ['1','2','3']
 l2 = ['4','5','6']
 
 for i in l1:
-    for j in l2:{
+    for j in l2:
         print(i,j)
-    }
+
+i = 1
+n = 2
+while(i<=5):
+    print(n," * ", i , " = " ,  n*i)
+    i +=1
