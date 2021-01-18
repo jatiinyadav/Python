e0adf9ce3ae0f3cec9af830ec2d32637b7f35d90
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-19 01:41:24 +0530

    update56

--- argument.py
+++ argument.py
@@ -11,3 +11,13 @@ list2 = [2,4,6,8]
 update(list1,list2)
 print("list1: ",list1)
 print("list2: ",list2)
+
+
+
+def calist(list3):
+
+    list3[0]=9
+    print("NEW List: ",list3)
+
+list3= [1,2,3,4,5]
+calist(list3)
