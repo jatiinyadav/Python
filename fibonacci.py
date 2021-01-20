4018a0b25db4f5343b465b5ff7cdb59d21d97b90
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-01-20 23:30:24 +0530

    update7-

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,18 @@
 x = int(input("Till where you want the Fibonacci series?\n"))
 
-def fib(n):
+def fib(x):
 
     a = 0
     b = 1
 
-    if (n<=0):
+    if (x<=0):
         print("N0. should be >= 1")
-    elif(n==1):
+    elif(x==1):
         print(a)
     else:
         print(a)
-        for i in range(1,n):
+        print(b)
+        for i in range(1,x):
             c = a + b
             a = b
             b = c
