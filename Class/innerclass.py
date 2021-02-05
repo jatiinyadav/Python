f06ba6bdbc9a3f78276aef8ca25fb5a8afc56a64
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-02-06 00:36:47 +0530

    update

--- Class/innerclass.py
+++ Class/innerclass.py
@@ -1,9 +1,9 @@
 class Student:
 
     def __init__(self):
-        self.name = input("Enter your name: ")
-        self.rollno = int(input("Enter your roll no. : "))
-        self.lap = self.Laptop
+        self.name = input("Name: ")
+        self.rollno = int(input("Roll no. : "))
+        self.lap = self.Laptop()                            #Object created in inner class
 
     def show(self):
         print("Your Name: {} \n Your Roll no. {} ".format(self.name, self.rollno))
@@ -21,11 +21,12 @@ class Student:
 
 s1 = Student()
 
-lap = s1.Laptop()
-
 s1.show()
 
-Student().Laptop().show()
+s1.lap.show()
 
-print(s1.show())
-print(Student().Laptop().show())
+# If we dont want to declare in inner class then for outer class declare a new object
+
+# lap1 = Student.Laptop()
+
+# print(lap1,show())
