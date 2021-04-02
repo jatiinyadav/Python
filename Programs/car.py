6cbd6a58afed846b43dabcdb11b14dce343b0343
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-04-02 22:44:36 +0530

    update

---
+++ Programs/car.py
@@ -0,0 +1,30 @@
+print("Use commands: \nquit\nstart \nstop \nhelp")
+
+command = ""
+started = False
+
+while command != "quit":
+    command = input("> ").lower()
+
+    if command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started moving.....")
+    
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped")
+        else:
+            started = False
+            print("Car stopped!!")
+
+    elif command == "help":
+        print("\nstart - car starts moving\nstop - car stops moving\nquit - quits the loop\n")
+
+    elif command == "quit":
+        break
+
+    else :
+        print("Use command words")
